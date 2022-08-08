b132fd4e79da5015347d8317b0069ac18b47799f
inspectpage <thuann.le64@gmail.com>
2022-08-08 02:58:45 -0400

    Added if else statements for win/loss result.

--- javascript.js
+++ javascript.js
@@ -5,6 +5,48 @@ function getComputerChoice(){
     return Computer;
 }
 
-function roundplay(playerselection){
+function roundplay(playerselection, computerSelection){
+    var selection = playerselection.toLowerCase();
+    var computerselect = computerSelection();
+    console.log(computerselect);
+    if (selection == 'rock' && computerselect == 'Rock'){
+        return "Tie!";
+    }
+    else if (selection == 'rock' && computerselect == 'Paper'){
+        return "You lose, paper beats rock!";
+    }
+    else if (selection == 'rock' && computerselect == 'Scissors'){
+        return "You win, rock beats scissors!";
+    }
 
+
+
+
+    else if (selection == 'scissors' && computerselect == 'Rock'){
+        return "You lose, rock beats scissors!";
+    }
+    else if (selection == 'scissors' && computerselect == 'Paper'){
+        return "You win, scissors beats paper!";
+    }
+    else if (selection == 'scissors' && computerselect == 'Scissors'){
+        return "Tie!";
+    }
+
+
+
+    else if (selection == 'paper' && computerselect == 'Rock'){
+        return "You win, paper beats rock!";
+    }
+    else if (selection == 'paper' && computerselect == 'Paper'){
+        return "Tie!";
+    }
+    else if (selection == 'paper' && computerselect == 'Scissors'){
+        return "You lose, scissors beat paper!";
+    }
+}
+
+function game(){
+    for (let i=0; i<5; i++){
+        roundplay(prompt(), getComputerChoice)
+    }
 }
